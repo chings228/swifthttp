ef9b9285e79daf8479606aded606c01b9928e2ca
tom <chings228@gmail.com>
2023-08-09 08:50:01 +0800

    Initial Commit

---
+++ HttpDemo/HttpDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HttpDemoApp.swift
+//  HttpDemo
+//
+//  Created by man ching chan on 9/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct HttpDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
